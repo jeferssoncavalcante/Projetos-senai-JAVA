33d58ed2e8fbbc57d7149c8a8057903eac4c4681
JEFERSON GOMES CAVALCANTE <74382480+jeferssoncavalcante@users.noreply.github.com>
2021-03-16 21:19:27 -0300

    Update EscreverJSON.java

--- EscreverJSON/src/escrever/json/rev1/EscreverJSON.java
+++ EscreverJSON/src/escrever/json/rev1/EscreverJSON.java
@@ -12,20 +12,19 @@ import java.nio.file.Paths;
 public class EscreverJSON {
 
 	public static void main(String[] args) throws IOException {
-		Path config2 = Paths.get("C:\\Users\\Acer\\Google Drive\\Classroom\\Senai\\Programação Web Back-End\\MeuPrograma\\teste.json");
+		Path config2 = Paths.get("C:\\CAMINHO\\DO\\ARQUIVO \\JSON");
 		//Path caminho = Paths.get("F:\\Users\\Acer\\Documents\\GitHub\\Projeto-Jogo-Memoria\\version2\\img\\marvel-dc\\");
 		
 		
 		BufferedWriter bw = null;
 		bw = Files.newBufferedWriter(config2, StandardCharsets.UTF_8);
 		
-		File arquivos = new File("F:\\Users\\Acer\\Documents\\GitHub\\Projeto-Jogo-Memoria\\version2\\img\\marvel-dc");
+		File arquivos = new File("C:\\CAMINHO\\DOS\\ARQUIVOS\\");
 			File[] file = arquivos.listFiles();
 					
 					for (int i = 0; i < file.length; i++) {
 						File f = file[i];
-						
-							bw.write("{" + "\"Id\":"+ "\"" + i + "\"" + ",\r\n" + "    \"Url\": \"./img/marvel-dc/" + f.getName() + "\"\r\n" + "},");
+						bw.write("{" + "\"Id\":"+ "\"" + i + "\"" + ",\r\n" + "    \"Url\": \"./img/marvel-dc/" + f.getName() + "\"\r\n" + "},");
 					}
 					
 				System.out.println("Fim !");
